d6ecf3c4886f9f876be87a1006207866f345db7c
killmerayr <dimaerm2006@gmail.com>
2025-11-23 04:13:48 +0700

    add corse requests

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -18,9 +18,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,3 +101,5 @@ SIMPLE_JWT = {
 
 # Custom user model
 AUTH_USER_MODEL = 'app.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
